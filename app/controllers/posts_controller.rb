cf31541e0d32b20a4c9dc251f4035ec20d700b00
陶梦迪 <taomengdi99@163.com>
2017-01-21 14:39:10 +0800

    use simple_format helper to decorate

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,9 @@ class PostsController < ApplicationController
   def new
     @group = Group.find(params[:group_id])
     @post = Post.new
+    if !current_user.is_member_of?(@group)
+      redirect_to group_path(@group),alert: '你还未加入这个讨论组，不允许发表内容！'
+    end
   end
 
   def create
@@ -17,9 +20,26 @@ class PostsController < ApplicationController
     end
   end
 
-  
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
 
-  private
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: 'Update Success'
+    else
+      render :edit
+    end 
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to account_posts_path, alert: 'Post deleted'
+  end
+    private
   def post_params
     params.require(:post).permit(:content)
   end
